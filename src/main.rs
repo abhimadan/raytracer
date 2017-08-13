20442ac1dbd70f744161a0a011b790fc8274ab62
Abhishek Madan <abhi.madan01@gmail.com>
2017-08-13 17:45:08 -0400

    Added supersampling to implement anti-aliasing. This also allows the image to be smaller while maintaining similar quality.

--- src/main.rs
+++ src/main.rs
@@ -122,43 +122,58 @@ fn main() {
 
   let mut file = File::create("out.ppm").expect("Failed to create PPM file.");
   writeln!(&mut file, "P3"); 
-  writeln!(&mut file, "{} {}", 801, 801);
+  writeln!(&mut file, "{} {}", 401, 401);
   writeln!(&mut file, "{}", 255);
 
-  for z in (-401..400).rev() {
-    for x in -400..401 {
-      let x = (x as i32 as f64)/20.0;
-      let z = (z as i32 as f64)/20.0;
+  for z in (-200..201).rev() {
+    for x in -200..201 {
+      let x_center = (x as i32 as f64)/10.0;
+      let z_center = (z as i32 as f64)/10.0;
 
-      let r = Ray::from(camera.clone(), Vec3::minus(&Vec3::from(x, 8.0, z), &camera));
+      let mut red = 0;
+      let mut green = 0;
+      let mut blue = 0;
 
-      let mut mindist = 1_000_000.0;
-      let mut pixel_colour = Vec3::from(0.0, 0.0, 0.0);
+      for dx in [-0.033, 0.0, 0.033].iter() {
+        for dz in [-0.033, 0.0, 0.033].iter() {
+          let x = x_center + dx;
+          let z = z_center + dz;
 
-      for &s in [&s1, &s2].iter() {
-        if let Some((point, len)) = s.intersect(&r) {
-          if (len < mindist) {
-            mindist = len;
+          let r = Ray::from(camera.clone(), Vec3::minus(&Vec3::from(x, 8.0, z), &camera));
 
-            let normal = Vec3::minus(&point, &s.center).normalize();
-            let to_light = Vec3::minus(&light, &point).normalize();
-            let shade = if Vec3::dot(&normal, &to_light) < 0.0001 {
-              0.0
-            } else {
-              Vec3::dot(&normal, &to_light)
-            };
+          let mut mindist = 1_000_000.0;
+          let mut pixel_colour = Vec3::from(0.0, 0.0, 0.0);
 
-            // Scale colour by ambient and diffuse coefficients,
-            // using Lambert shading for diffuse amount.
-            let intensity = ambient + diffuse*shade;
-            pixel_colour = s.colour.scale(intensity);
+          for &s in [&s1, &s2].iter() {
+            if let Some((point, len)) = s.intersect(&r) {
+              if (len < mindist) {
+                mindist = len;
+
+                let normal = Vec3::minus(&point, &s.center).normalize();
+                let to_light = Vec3::minus(&light, &point).normalize();
+                let shade = if Vec3::dot(&normal, &to_light) < 0.0001 {
+                  0.0
+                } else {
+                  Vec3::dot(&normal, &to_light)
+                };
+
+                // Scale colour by ambient and diffuse coefficients,
+                // using Lambert shading for diffuse amount.
+                let intensity = ambient + diffuse*shade;
+                pixel_colour = s.colour.scale(intensity);
+              }
+            }
           }
+
+          red += (pixel_colour.x * 255.0) as i32;
+          green += (pixel_colour.y * 255.0) as i32;
+          blue += (pixel_colour.z * 255.0) as i32;
         }
       }
 
-      let red = (pixel_colour.x * 255.0) as i32;
-      let green = (pixel_colour.y * 255.0) as i32;
-      let blue = (pixel_colour.z * 255.0) as i32;
+      red /= 9;
+      green /= 9;
+      blue /= 9;
       write!(&mut file, "{} {} {}\t", red, green, blue);
     }
     writeln!(&mut file, "");
